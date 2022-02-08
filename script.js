9f4364d450f69d8a9075391f3b2ce8df9d96896b
michelllle-liu <michelle.01.liu@outlook.com>
2022-02-07 23:21:43 -0400

    Add computerPlay and playRound functions

---
+++ script.js
@@ -0,0 +1,48 @@
+function computerPlay(){
+    const moves = ["Rock", "Paper", "Scissors"]
+
+    const random = Math.floor(Math.random() * moves.length);
+
+    return (moves[random]);
+}
+
+console.log(computerPlay());
+
+function playRound (playerSelection, computerSelection){
+    switch (playerSelection.toUpperCase()){
+        case "ROCK":
+            if (computerSelection === "Rock"){
+                return "Tie!";
+            }
+            else if (computerSelection === "Paper"){
+                return "You lose! Paper beats Rock";
+            }
+            else{
+                return "You win! Rock beats Scissors";
+            }
+        case "PAPER":
+            if (computerSelection === "Rock"){
+                return "You win! Paper beats Rock";
+            }
+            else if (computerSelection === "Paper"){
+                return "Tie!";
+            }
+            else{
+                return "You lose! Scissors beats Paper";
+            }
+        case "SCISSORS":
+            if (computerSelection === "Rock"){
+                return "You lose! Rock beats Scissors";
+            }
+            else if (computerSelection === "Paper"){
+                return "You win! Scissors beats Paper";
+            }
+            else{
+                return "Tie!";
+            }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
